6c5c68add712ae4c68388387a6ee95f12e5b87ac
boesner <45736940+boesner@users.noreply.github.com>
2021-12-10 20:05:41 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+       $('.fadeOnLoad').fadeIn(4000);
+       $("html,body").animate({scrollTop: 1000}, 1000);
+});
+
+
+$(".fa-envelope").click(function(event) {
+  $(".message").fadeIn(1000);
+  $(".btn-outline-info").text("send");
+});
+
+$(".btn-outline-info").click(function(event){
+  $(".btn-outline-info").text("Thank you for your message!");
+  $(".message").delay(1000).fadeOut(2000);
+
+});
+
+$(document).ready(function(){
+     $("h1").animate({scrollTop: 100000}, 1000);
+});
